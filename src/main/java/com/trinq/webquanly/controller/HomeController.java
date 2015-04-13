469b183655aab43dccfcd7df2123db24bbaff15d
Anonymous <triquang13@gmail.com>
2015-04-13 09:26:34 +0000

    init

---
+++ src/main/java/com/trinq/webquanly/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.trinq.webquanly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/welcome")
+	public String welcome(Model model) {
+		model.addAttribute("greeting", "Welcome to webpage ");
+		model.addAttribute("tagline", "The one and only amazing web store");
+
+		return "welcome";
+	}
+
+}
